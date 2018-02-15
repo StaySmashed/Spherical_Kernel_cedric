0532f27ec0f45c663fa4b54d9618f0ae5874221f
ATechnoHazard <amolele@gmail.com>
2018-02-15 19:36:47 +0000

    Initial ramdisk changes
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -25,13 +25,17 @@ is_slot_device=0;
 
 ## AnyKernel permissions
 # set permissions for included ramdisk files
-chmod -R 755 $ramdisk
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
-
+insert_line init.rc "init.sphere.rc" after "import /init.usb.rc" "import /init.sphere.rc";
+replace_line "/init.qcom.rc" "write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 1401600" "write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 1094400";
+replace_line "/init.qcom.rc" "85 1401600:80" "1 960000:85 1094400:90 1344000:80";
+replace_line "/init.qcom.rc" "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 652800" "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 960000";
 # end ramdisk changes
 
 write_boot;
