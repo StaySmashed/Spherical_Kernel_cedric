944c97a3c8d7fe45f4c0e9b8f4987557654d7cf7
ATechnoHazard <amolele@gmail.com>
2018-02-15 20:55:50 +0530

    Send a jorbis sticker as a separator
    Huihuihuihui
    
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- build.sh
+++ build.sh
@@ -14,8 +14,13 @@ sudo apt-get update --quiet
 sudo apt-get install --yes build-essential bc kernel-package libncurses5-dev bzip2 liblz4-tool git curl
 
 chmod a+x ./Builds/deploy-tg.sh
-chmod a+x ./Builds/common
+#chmod a+x ./Builds/common
 chmod a+x ./*
+
+#source ./Builds/common
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker -d sticker="CAADBQADIgADTBCSGjYU8tTvyHO6Ag"  -d chat_id=@nubci
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Semaphore CI build for spherical kernel started" -d chat_id=@nubci
+
 ## Copy this script inside the kernel directory
 KERNEL_DIR=$PWD
 KERNEL_TOOLCHAIN=$KERNEL_DIR/toolchain/bin/arm-eabi-
@@ -26,7 +31,7 @@ KERNEL_DEFCONFIG=cedric_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS="-j$(nproc --all)"
 ANY_KERNEL2_DIR=$KERNEL_DIR/AnyKernel2/
-FINAL_KERNEL_ZIP=Spaghet-7.1.x_$V-_CEDRIC.zip
+FINAL_KERNEL_ZIP=Spherical_Kernel-7.1.x_$V-_CEDRIC.zip
 export ZIP_NAME=$FINAL_KERNEL_ZIP
 export LOCALVERSION="_$V"
 MAKE="./makeparallel"
@@ -56,18 +61,18 @@ echo -e "***********************************************$nocol"
 $DTBTOOL/dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/dtb -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/qcom/
 
 echo "**** Verify zImage & dtb ****"
-ls $KERNEL_DIR/arch/arm/boot/Image.gz
+ls $KERNEL_DIR/arch/arm/boot/zImage
 ls $KERNEL_DIR/arch/arm/boot/dtb
 
 echo "**** Verifying Anyernel2 Directory ****"
 ls $ANY_KERNEL2_DIR
 echo "**** Removing leftovers ****"
 rm -rf $ANY_KERNEL2_DIR/dtb
-rm -rf $ANY_KERNEL2_DIR/Image.gz
+rm -rf $ANY_KERNEL2_DIR/zImage
 rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
 
 echo "**** Copying zImage ****"
-cp $KERNEL_DIR/arch/arm/boot/Image.gz $ANY_KERNEL2_DIR/
+cp $KERNEL_DIR/arch/arm/boot/zImage $ANY_KERNEL2_DIR/
 echo "**** Copying dtb ****"
 cp $KERNEL_DIR/arch/arm/boot/dtb $ANY_KERNEL2_DIR/
 
@@ -81,7 +86,7 @@ echo "**** Good Bye!! ****"
 cd $KERNEL_DIR
 rm -rf arch/arm/boot/dtb
 rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
-rm -rf $ANY_KERNEL2_DIR/Image.gz
+rm -rf $ANY_KERNEL2_DIR/zImage
 rm -rf $ANY_KERNEL2_DIR/dtb
 
 BUILD_END=$(date +"%s")
