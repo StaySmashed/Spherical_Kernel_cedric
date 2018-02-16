e22060ded1bd99247653814ea6d9e20b518ef5da
ATechnoHazard <amolele@gmail.com>
2018-02-16 19:43:34 +0000

    defconfig: enable FRANDOM
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- Builds/deploy-tg.sh
+++ Builds/deploy-tg.sh
@@ -2,7 +2,7 @@
 source common
 message="Semaphore CI build completed with the latest commit -"
 
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$message $(git log --pretty=format:'%h : %s' -1)" -d chat_id=$CHAT_ID
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$message $(git log --pretty=format:'%h : %s' -1)" -d chat_id=@nubci
 
 
 #curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$(git log --pretty=format:'%h : %s' -1)" -d chat_id=$CHAT_ID
